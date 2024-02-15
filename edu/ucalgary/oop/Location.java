0a2471f8499485191bbf54a8dcdde09b54c61718
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-14 18:39:55 -0700

    create Location class

---
+++ edu/ucalgary/oop/Location.java
@@ -0,0 +1,10 @@
+package edu.ucalgary.oop;
+
+import java.util.List;
+
+public class Location {
+   private String name;
+   private String address;
+   private List<DisasterVictim> occupants;
+   private List<Supply> supplies;
+}
