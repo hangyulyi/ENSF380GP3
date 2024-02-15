59a3e6e4a25d16cb38e8f3b0764afa100817cf02
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-14 18:37:32 -0700

    create ReliefService class

---
+++ edu/ucalgary/oop/ReliefService.java
@@ -0,0 +1,9 @@
+package edu.ucalgary.oop;
+
+public class ReliefService {
+   private Inquirer inquirer;
+   private DisasterVictim missingPerson;
+   private String dateOfInquiry;
+   private String infoProvided;
+   private Location lastKnownLocation;
+}
