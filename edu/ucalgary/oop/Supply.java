cfe5eb8d009801fdd5fbcf6ee4bbd951ab1e1092
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-14 21:22:36 -0700

    finish Supply class

--- edu/ucalgary/oop/Supply.java
+++ edu/ucalgary/oop/Supply.java
@@ -4,4 +4,22 @@ public class Supply {
    private String type;
    private int quantity;
 
+   /* Constructor */
+   public Supply(String type, int quantity) {
+      this.type = type;
+      this.quantity = quantity;
+   }
+
+   /* Setter and Getter */
+   public void setType(String type) {
+      this.type = type;
+   }
+
+   public void setQuantity(int quantity) {
+      this.quantity = quantity;
+   }
+
+   public String getType() { return this.type; }
+   public int getQuantity() { return this.quantity; }
+
 }
