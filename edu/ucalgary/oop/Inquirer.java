7e861605c144a179b14480cfe8807309ea973d36
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-15 15:21:39 -0700

    fix Inquirer to match test

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -17,5 +17,5 @@ public class Inquirer {
    public String getFirstName() { return this.FIRST_NAME; }
    public String getLastName() { return this.LAST_NAME; }
    public String getInfo() { return this.INFO; }
-   public String getServicesPhone() { return this.SERVICES_PHONE; }
+   public String getServicesPhoneNum() { return this.SERVICES_PHONE; }
 }
