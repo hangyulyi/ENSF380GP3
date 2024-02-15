49778e2e6a8238aef398974047a5bb49c346a6ad
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-15 15:30:49 -0700

    add setter/getter for Location class

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -5,6 +5,37 @@ import java.util.List;
 public class Location {
    private String name;
    private String address;
-   private List<DisasterVictim> occupants;
-   private List<Supply> supplies;
+   private DisasterVictim[] occupants;
+   private Supply[] supplies;
+
+   /* Constructor */
+   public Location(String name, String address) {
+      this.name = name;
+      this.address = address;
+   }
+
+   /* Setters */
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+
+   public void setOccupants(DisasterVictim[] occupants) {
+      this.occupants = occupants;
+   }
+
+   public void setSupplies(Supply[] supplies) {
+      this.supplies = supplies;
+   }
+
+   /* Getters */
+   public String getName() { return this.name; }
+   public String getAddress() { return this.address; }
+   public DisasterVictim[] getOccupants() { return this.occupants; }
+   public Supply[] getSupplies() { return this.supplies; }
+
+   
 }
