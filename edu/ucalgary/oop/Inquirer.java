95335b0c0afce8600dbd945f5cbd3c6a0e52e3a2
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-14 21:26:34 -0700

    finish Inquirer class

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -5,4 +5,17 @@ public class Inquirer {
    private String LAST_NAME;
    private String INFO;
    private String SERVICES_PHONE;
+
+   public Inquirer(String FIRST_NAME, String LAST_NAME, String SERVICES_PHONE, String INFO) {
+      this.FIRST_NAME = FIRST_NAME;
+      this.LAST_NAME = LAST_NAME;
+      this.SERVICES_PHONE = SERVICES_PHONE;
+      this.INFO = INFO;
+   }
+
+   /* Getter */
+   public String getFirstName() { return this.FIRST_NAME; }
+   public String getLastName() { return this.LAST_NAME; }
+   public String getInfo() { return this.INFO; }
+   public String getServicesPhone() { return this.SERVICES_PHONE; }
 }
