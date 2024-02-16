39f4842b0c4a87c4a57c165519f1d6d5905c39e7
TRUNG TUAN MAI <maitrungtuan2002@gmail.com>
2024-02-15 21:06:40 -0700

    location passed test,

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,17 +1,19 @@
 package edu.ucalgary.oop;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Location {
    private String name;
    private String address;
-   private DisasterVictim[] occupants;
-   private Supply[] supplies;
+   private ArrayList<DisasterVictim> occupants;
+   private ArrayList<Supply> supplies;
 
    /* Constructor */
    public Location(String name, String address) {
       this.name = name;
       this.address = address;
+      this.occupants = new ArrayList<>();
+      this.supplies = new ArrayList<>();
    }
 
    /* Setters */
@@ -23,19 +25,33 @@ public class Location {
       this.address = address;
    }
 
-   public void setOccupants(DisasterVictim[] occupants) {
+   public void setOccupants(ArrayList<DisasterVictim> occupants) {
       this.occupants = occupants;
    }
 
-   public void setSupplies(Supply[] supplies) {
+   public void setSupplies(ArrayList<Supply> supplies) {
       this.supplies = supplies;
    }
 
    /* Getters */
    public String getName() { return this.name; }
    public String getAddress() { return this.address; }
-   public DisasterVictim[] getOccupants() { return this.occupants; }
-   public Supply[] getSupplies() { return this.supplies; }
+   public ArrayList<DisasterVictim> getOccupants(){ return this.occupants; }
+   public ArrayList<Supply> getSupplies() { return this.supplies; }
+   public void addOccupant(DisasterVictim occupant) {
+      occupants.add(occupant);
+   }
 
+   public void removeOccupant(DisasterVictim occupant) {
+      occupants.remove(occupant);
+   }
+
+   public void addSupply(Supply supply) {
+      supplies.add(supply);
+   }
+
+   public void removeSupply(Supply supply) {
+      supplies.remove(supply);
+   }
    
 }
