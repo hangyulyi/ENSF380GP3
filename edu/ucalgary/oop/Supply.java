16e1294e544065f7f9ceb4e315eb368ed3c96f58
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-16 14:04:47 -0700

    add documentation for Supply

--- edu/ucalgary/oop/Supply.java
+++ edu/ucalgary/oop/Supply.java
@@ -1,5 +1,10 @@
 package edu.ucalgary.oop;
 
+/**
+ * This represents a Supply in the system.
+ * 
+ * Users can indicate the supply and quantity of the Supply object.
+ */
 public class Supply {
    private String type;
    private int quantity;
@@ -10,7 +15,7 @@ public class Supply {
       this.quantity = quantity;
    }
 
-   /* Setter and Getter */
+   /* Setters */
    public void setType(String type) {
       this.type = type;
    }
@@ -19,6 +24,7 @@ public class Supply {
       this.quantity = quantity;
    }
 
+   /* Getters */
    public String getType() { return this.type; }
    public int getQuantity() { return this.quantity; }
 
