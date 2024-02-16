4950cebe89443a66c7882b3f8124e3db2e705ef0
TRUNG TUAN MAI <maitrungtuan2002@gmail.com>
2024-02-15 21:25:57 -0700

    relief service passed test

--- edu/ucalgary/oop/ReliefService.java
+++ edu/ucalgary/oop/ReliefService.java
@@ -6,4 +6,64 @@ public class ReliefService {
    private String dateOfInquiry;
    private String infoProvided;
    private Location lastKnownLocation;
+
+   public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+      this.inquirer = inquirer;
+      this.missingPerson = missingPerson;
+      this.dateOfInquiry = dateOfInquiry;
+      this.infoProvided = infoProvided;
+      this.lastKnownLocation = lastKnownLocation;
+   }
+
+   public Inquirer getInquirer() {
+      return inquirer;
+   }
+
+   public void setInquirer(Inquirer inquirer) {
+      this.inquirer = inquirer;
+   }
+
+   public DisasterVictim getMissingPerson() {
+      return missingPerson;
+   }
+
+   public void setMissingPerson(DisasterVictim missingPerson) {
+      this.missingPerson = missingPerson;
+   }
+
+   public String getDateOfInquiry() {
+      return dateOfInquiry;
+   }
+
+   public void setDateOfInquiry(String dateOfInquiry) {
+      if (!isValidDateFormat(dateOfInquiry)) {
+         throw new IllegalArgumentException("Invalid date format. Date should be in the format YYYY-MM-DD.");
+      }
+      this.dateOfInquiry = dateOfInquiry;
+   }
+   private boolean isValidDateFormat(String date) {
+      return date.matches("\\d{4}-\\d{2}-\\d{2}");
+   }
+
+   public String getInfoProvided() {
+      return infoProvided;
+   }
+
+   public void setInfoProvided(String infoProvided) {
+      this.infoProvided = infoProvided;
+   }
+
+   public Location getLastKnownLocation() {
+      return lastKnownLocation;
+   }
+
+   public void setLastKnownLocation(Location lastKnownLocation) {
+      this.lastKnownLocation = lastKnownLocation;
+   }
+
+   public String getLogDetails() {
+      // Assuming Inquirer and DisasterVictim have appropriate getName() methods
+      return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: " + missingPerson.getFirstName() + ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: " + infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
+   }
+
 }
